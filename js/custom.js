d889f5b2f766b7e9fb317bad9c70f3c5f1d44216
Ilya Bashilov <ilya.bashilov@simbirsoft.com>
2018-09-17 14:05:02 +0400

    верстка табов урок 8

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,12 @@
-
 console.log('haya!!');
 
-
-
-	/* var hf = function(){
-		var h_header = $('header').height();
-		var h_footer = $('footer').height();
-		$('.content').css({
-			'paddingTop': h_header,
-			'paddingBottom': h_footer
-		});
-	}
-
-	$(window).bind('load resize', hf);
- */
-
-   
-
-
+$(function () {
+  $('.tabs a').click(function () {
+    $(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+    $(this).parent().siblings().removeClass('active');
+    var id = $(this).attr('href');
+    $(id).removeClass('hide');
+    $(this).parent().addClass('active');
+    return false
+  });
+});
